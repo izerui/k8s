0bd7aeb54bf3737a8b0d07879a7066b183263efa
serv <yifeng2000@126.com>
2020-08-03 18:53:38 +0800

    nginx.ingress.kubernetes.io/auth-type: basic     nginx.ingress.kubernetes.io/auth-secret: basic-auth-secret     nginx.ingress.kubernetes.io/auth-realm: "Authentication Required"

--- simple_jenkins_build_init.sh
+++ simple_jenkins_build_init.sh
@@ -1,8 +1,9 @@
-export APP_NAME=eureka-config-server
+cd /home/k8s/
+git pull
+export APP_NAME=admin-server
 export APP_VERSION=1.0.0
-export APP_PORT=3030
+export APP_PORT=33011
 export PROFILE=dev
 export BUILD_SH=jar.sh
 export REGISTRY_GROUP=harbor.yj2025.com
-cd /home/k8s/
 sh ./build.sh
