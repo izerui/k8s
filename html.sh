20e82baef24736cba6cbb1d04a1384b8b32714c1
serv <yifeng2000@126.com>
2020-08-04 10:49:24 +0800

    12

--- html.sh
+++ html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HTML_FOLDER=docker/${APP_NAME}/target
-tar zxvf docker/${APP_NAME}/app.tar.gz -C docker/${APP_NAME}/target
+tar zxvf docker/${APP_NAME}/app.tar.gz -C ${HTML_FOLDER}
 echo ":::::: 生成 Dockerfile"
 if [ ! -f "tpl/${APP_NAME}/Dockerfile" ];then
 DOCKER_TPL_FILE=tpl/default/Dockerfile
