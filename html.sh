38acfcbe1be345ea52f1f8c0aa295f18bf2dec28
serv <yifeng2000@126.com>
2020-08-04 11:57:54 +0800

    12

--- html.sh
+++ html.sh
@@ -2,10 +2,10 @@
 HTML_FOLDER=docker/${PROFILE}/${APP_NAME}/target
 tar zxvf docker/${PROFILE}/${APP_NAME}/app.tar.gz -C ${HTML_FOLDER}
 echo ":::::: 生成 Dockerfile"
-if [ ! -f "tpl/${APP_NAME}/Dockerfile" ];then
+if [ ! -f "./tpl/${PROFILE}/${APP_NAME}/Dockerfile" ];then
 DOCKER_TPL_FILE=tpl/default/Dockerfile
 else
-DOCKER_TPL_FILE=tpl/${APP_NAME}/Dockerfile
+DOCKER_TPL_FILE=./tpl/${PROFILE}/${APP_NAME}/Dockerfile
 fi
 echo ":::::: 使用 ${DOCKER_TPL_FILE}"
 eval "cat <<EOF
--- jar.sh
+++ jar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 JAR_FILE=docker/${PROFILE}/${APP_NAME}/${APP_NAME}-exec.jar
 echo ":::::: 生成 Dockerfile"
-if [ ! -f "tpl/${APP_NAME}/Dockerfile" ];then
+if [ ! -f "./tpl/${PROFILE}/${APP_NAME}/Dockerfile" ];then
 DOCKER_TPL_FILE=tpl/default/Dockerfile
 else
-DOCKER_TPL_FILE=tpl/${APP_NAME}/Dockerfile
+DOCKER_TPL_FILE=./tpl/${PROFILE}/${APP_NAME}/Dockerfile
 fi
 echo ":::::: 使用 ${DOCKER_TPL_FILE}"
 eval "cat <<EOF
