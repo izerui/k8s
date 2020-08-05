076bb0033e21718e8d2e3cdff39c513aa2348b06
serv <yifeng2000@126.com>
2020-08-05 15:43:04 +0800

    12

--- init/cert/init.sh
+++ init/cert/init.sh
@@ -2,6 +2,6 @@
 kubectl delete secret yj2025-secret -n default
 kubectl delete secret yj2025-secret -n dev
 kubectl delete secret yj2025-secret -n test
-kubectl create secret tls yj2025-secret --cert server.pem --key server.key -n default
-kubectl create secret tls yj2025-secret --cert server.pem --key server.key -n dev
-kubectl create secret tls yj2025-secret --cert server.pem --key server.key -n test
+kubectl create secret tls yj2025-secret --cert tls.crt --key tls.key -n default
+kubectl create secret tls yj2025-secret --cert tls.crt --key tls.key -n dev
+kubectl create secret tls yj2025-secret --cert tls.crt --key tls.key -n test
